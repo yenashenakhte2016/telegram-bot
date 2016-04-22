4a318e3b1b730f36da8f22b2f3a58940af14f4e1
Kaisar Arkhan <yukinagato@protonmail.com>
2016-04-22 16:16:09 +0700

    Ignore Teleseeds commands to echo plugin

--- plugins/echo.py
+++ plugins/echo.py
@@ -3,7 +3,12 @@ def main(tg_api):  # This is where plugin_handler will send msg
     if 'reply_to_message' in tg_api.msg:
         tg_api.send_message(tg_api.msg['reply_to_message']['text'])
     elif len(tg_api.msg['match']) > 1:
-        tg_api.send_message(tg_api.msg['match'][1])
+        message = tg_api.msg['match'][1]
+
+        if message.startswith('#') # Ignore message, because may conflict with TeleSeeds commands
+            return
+
+        tg_api.send_message(message)
     else:
         tg_api.send_message("What do I echo?")
 
