6b649f65a4ba4a1d27997931ab7fb14d0a7764c8
Kaisar Arkhan <yukinagato@protonmail.com>
2016-04-22 16:06:55 +0700

    Change filename, and added the standard boilerplate for start the program

--- hitagi.py
+++ hitagi.py
@@ -1,8 +1,14 @@
+#!/bin/env python3
+
 from bot import Bot
 import util
 
 config = util.ConfigUtils()  # Create config object
 bot = Bot(config)  # Create bot object
 
-while True:  # Main loop
-    bot.get_update()
+def main():
+    while True:  # Main loop
+        bot.get_update()
+
+if __name__ == '__main__':
+    main()
