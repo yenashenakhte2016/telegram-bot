3bd6545a1f704ced3aad3d8bda4763c40cabc69a
Walid <topbakuhatsu@protonmail.ch>
2016-05-11 19:03:59 +0000

    Logging/Some cleanup to tgapi

--- tgapi.py
+++ tgapi.py
@@ -1,6 +1,5 @@
-import sys
 from functools import partial
-
+import logging
 import util
 
 
@@ -9,6 +8,7 @@ class TelegramApi:
         self.msg = message
         self.package = package
         self.plugin_id = plugin_id
+        self.log = logging.getLogger(__name__)
         self.send_photo = partial(self.send_file, 'sendPhoto')
         self.send_audio = partial(self.send_file, 'sendAudio')
         self.send_document = partial(self.send_file, 'sendDocument')
@@ -18,9 +18,6 @@ class TelegramApi:
         self.get_user_profile_photos = partial(self.send_text, 'getUserProfilePhotos')
         self.kick_chat_member = partial(self.send_text, 'kickChatMember', chat_id=self.msg['chat']['id'])
         self.unban_chat_member = partial(self.send_text, 'unbanChatMember', chat_id=self.msg['chat']['id'])
-        self.get_user_profile_photos = partial(self.send_text,
-                                               'getUserProfilePhotos',
-                                               chat_id=self.msg['chat']['id'])
         self.edit_message_text = partial(self.send_text, 'editMessageText')
         self.edit_message_caption = partial(self.send_text, 'editMessageCaption')
         self.edit_message_reply_markup = partial(self.send_text, 'editMessageReplyMarkup')
@@ -29,16 +26,20 @@ class TelegramApi:
         self.send_contact = partial(self.send_text, 'sendContact')
         self.forward_message = partial(self.send_text, 'forwardMessage')
         self.answer_callback_query = partial(self.send_text, 'answerCallbackQuery')
+        self.get_user_profile_photos = partial(self.send_text,
+                                               'getUserProfilePhotos',
+                                               chat_id=self.msg['chat']['id'])
 
-    def send_chat_action(self, action, chat_id=0):
-        if type(chat_id) != int or chat_id is 0:
+    def send_chat_action(self, action, chat_id=False):
+        if not chat_id:
             chat_id = self.msg['chat']['id']
         package = dict()
         package['data'] = {
             'chat_id': chat_id,
             'action': action
         }
-        return send_method(self.package[0], self.package[2], package, 'sendChatAction')
+        self.log.info('sendChatAction "{}" to {}'.format(action, chat_id))
+        return self.send_method(package, 'sendChatAction')
 
     def send_file(self, method, file=None, **kwargs):
         package = dict()
@@ -51,7 +52,8 @@ class TelegramApi:
             package['files'] = file
         for k, v in kwargs.items():
             package['data'][k] = v
-        return send_method(self.package[0], self.package[2], package, method)
+        self.log.info('{} to {}'.format(method, package['data']['chat_id']))
+        return self.send_method(package, method)
 
     def send_text(self, method, user_id, **kwargs):
         package = dict()
@@ -60,7 +62,8 @@ class TelegramApi:
         }
         for k, v in kwargs.items():
             package['data'][k] = v
-        return send_method(self.package[0], self.package[2], package, package, method)
+        self.log.info('{} to {}'.format(method, package['data']['chat_id']))
+        return self.send_method(package, package, method)
 
     def send_message(self, text, flag_message=False, flag_user_id=None, **kwargs):
         package = dict()
@@ -73,19 +76,28 @@ class TelegramApi:
             package['data'].update({'reply_to_message_id': self.msg['message_id']})
         for k, v in kwargs.items():
             package['data'][k] = v
+        self.log.info('sendMessage to {} and flag_message={}'.format(package['data']['chat_id'], flag_message))
         if flag_message:
-            msg = send_method(self.package[0], self.package[2], package, 'sendMessage')
+            msg = self.send_method(package, 'sendMessage')
             self.flag_message(message_id=msg['message_id'], chat_id=msg['chat']['id'], user_id=flag_user_id)
             return msg
         else:
-            return send_method(self.package[0], self.package[2], package, 'sendMessage')
+            return self.send_method(package, 'sendMessage')
+
+    def send_method(self, data, method, base_url='{0}{1}{2}'):  # General function for sending methods
+        data['url'] = base_url.format(self.package[0][0], self.package[0][1], method)
+        response = util.post(data, self.package[2]).json()
+        if not response['ok']:
+            self.log.error('Error received in response: {}'.format(response))
+        return response
 
     def get_file(self, file_id, download=False):
         package = dict()
         package['data'] = {
             'file_id': file_id
         }
-        response = send_method(self.package[0], self.package[2], package, 'getFile')
+        response = self.send_method(package, 'getFile')
+        self.log.info('Grabbing file {} and download={}'.format(file_id, download))
         if download:
             return self.download_file(response)
         else:
@@ -99,9 +111,10 @@ class TelegramApi:
             name = None
         file_name = util.name_file(file_object['file_id'], name)
         file_path = util.fetch_file(url, 'data/files/{}'.format(file_name), self.package[2])
+        self.log.info('Downloaded file {} in /data/files/'.format(file_name, file_path))
         return file_path
 
-    def flag_message(self, plugin=None, message_id=None, chat_id=None, user_id=None):
+    def flag_message(self, plugin=None, message_id=None, chat_id=None, user_id=None):  # Flags a message in the DB
         if not plugin:
             plugin_id = self.plugin_id
         else:
@@ -116,16 +129,3 @@ class TelegramApi:
             self.package[4].delete('flagged_messages', [('chat_id', chat_id)])
         if message_id and chat_id:
             self.package[4].insert('flagged_messages', [plugin_id, message_id, chat_id, user_id])
-
-
-def send_method(url, session, returned_value, method, base_url='{0}{1}{2}'):  # If dict is returned
-    package = {'url': base_url.format(url[0], url[1], method)}
-    for k, v in returned_value.items():
-        package[k] = v
-    response = util.post(package, session).json()
-    if response['ok']:
-        return response['result']
-    else:
-        print("There seems to be an error in the response :(")
-        print(response)
-        sys.exit()
