b593bd75c48062c4413f37064cecef758866574b
Walid <topbakuhatsu@protonmail.ch>
2016-05-14 16:11:48 +0000

    Clean up

--- hitagi.py
+++ hitagi.py
@@ -1,18 +1,42 @@
 #!/bin/env python3
 
-from bot import Bot
-import os
+import concurrent.futures
+import logging
+import time
+
+import util
+from route_message import RouteMessage
+
+update_id = 0
+config = util.ConfigUtils()  # Create config file object
+package = util.init_package(config)
+log = logging.getLogger(__name__)  # init log
 
 
 def main():
-    if not os.path.exists('data'):
-        os.makedirs('data/files')
-        os.makedirs('data/logs')
-    x = 0
-    bot = Bot()  # Create bot object
-    while True:
-        x = bot.get_update(x)
+    global update_id
+    url = "{}{}getUpdates?offset={}".format(package[0][0], package[0][1], update_id)
+    response = util.fetch(url, package[2])
+    try:
+        response = response.json()
+    except AttributeError:
+        log.error("Error parsing Telegram response\nResponse: {}".format(response.text))
+        time.sleep(5)
+        return
+    if response['ok'] and response['result']:  # Response ok and contains results
+        update_id = response['result'][-1]['update_id'] + 1
+        log.debug("Set update_id to {}".format(update_id))
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=config.workers)  # See docs
+        for result in response['result']:  # Loop through result
+            if_old = int(time.time()) - int(result['message']['date']) >= 180  # check if message is older than 3 min
+            log.debug("Routing message {} - Old: {}".format(result['message']['message_id'], if_old))
+            executor.submit(RouteMessage, result['message'], package, check_db_only=if_old)  # submit to executor
+        executor.shutdown(wait=False)  # returns immediately, subprocesses will close by themselves
+    elif not response['ok']:  # Response not ok
+        log.error('Response not OK\nResponse: {}'.format(response))
+    time.sleep(config.sleep)  # Sleep for time defined in config
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
--- util.py
+++ util.py
@@ -1,6 +1,11 @@
 import re
 import shutil
 import requests
+from db import Database
+import sys
+import os
+import json
+import logging
 
 
 class ConfigUtils:
@@ -57,3 +62,80 @@ def name_file(file_id, file_name):
         return file_id + match[0]
     else:
         return str(file_id)
+
+
+def init_package(config):  # Creates the package that's passed around, replaced eventually probably
+    session = requests.session()  # Creates session, used for ALL requests to telegram
+    bot_info = get_me(config.token, session)  # this stores the getMe object
+    database = init_db()  # Stores the database object
+    plugins = init_plugins(database, config.plugins)  # Stores all plugins
+    package = (('https://api.telegram.org/', 'bot{}/'.format(config.token)),  # URL [0]
+               bot_info,  # bot_info [1]
+               session,  # session [2]
+               plugins,  # plugins [3]
+               database)  # database [4]
+    return package
+
+
+def init_db():  # Creates the DB object and sets up hierarchy
+    log = logging.getLogger(__name__)  # init log
+    if not os.path.exists('data'):  # All data should be placed here
+        os.makedirs('data/files')
+        os.makedirs('data/logs')
+    log.debug('Creating/Opening database')
+    db = Database('bot')
+    log.debug('Creating plugins table')
+    db.create_table('plugins', [('plugin_id', 'INT PRIMARY KEY NOT NULL'),  # Creates plugin table
+                                ('plugin_name', 'TEXT'),
+                                ('pretty_name', 'TEXT'),
+                                ('description', 'TEXT'),
+                                ('usage', 'TEXT'), ], overwrite=True)
+    log.debug('Created flagged_messages table')
+    db.create_table('flagged_messages', [('plugin_id', 'INT'),  # Creates flagged messages table
+                                         ('message_id', 'INT'),
+                                         ('chat_id', 'INT'),
+                                         ('user_id', 'INT')])
+    log.debug('Successfully initialized the database')
+    return db
+
+
+def init_plugins(db, plugin_list):
+    log = logging.getLogger(__name__)  # init log
+    log.info('Initializing plugins...')
+    plugins = list()
+    for plugin_id, plugin_name in enumerate(plugin_list):  # Read plugins from the config file
+        plugin = __import__('plugins', fromlist=[plugin_name])  # Import it from the plugins folder
+        plugins.append(getattr(plugin, plugin_name))  # Stores plugin objects in a dictionary
+        if 'name' not in plugins[plugin_id].plugin_info:  # Check for name in plugin arguments
+            log.warning.append('Warning: {} is missing a pretty name. Falling back to filename.'.format(plugin_name))
+            plugins[plugin_id].plugin_info['name'] = plugin_name
+        pretty_name = plugins[plugin_id].plugin_info['name']
+        if 'desc' not in plugins[plugin_id].plugin_info:  # Check for description in plugin arguments
+            log.warning.append('Warning: {} is missing a description.'.format(plugin_name))
+            plugins[plugin_id].plugin_info['desc'] = None
+        description = plugins[plugin_id].plugin_info['desc']
+        if 'usage' not in plugins[plugin_id].plugin_info:  # Check for usage in plugin arguments
+            log.warning.append('Warning: {} is missing usage.'.format(plugin_name))
+            plugins[plugin_id].plugin_info['usage'] = None
+        usage = json.dumps(plugins[plugin_id].plugin_info['usage'])  # Stores usage as json
+        log.info('Loaded Plugin: {} - {}({}) - {} - {}'.format(plugin_id,
+                                                               plugin_name,
+                                                               pretty_name,
+                                                               description,
+                                                               usage))
+        db.insert('plugins', [plugin_id, plugin_name, pretty_name, description, usage])  # Insert into DB
+    log.info('Finished initializing plugins')
+    return tuple(plugins)
+
+
+def get_me(token, session):  # getMe
+    log = logging.getLogger(__name__)  # init log
+    log.info('Grabbing bot info')
+    url = "https://api.telegram.org/bot{}/getMe".format(token)  # Set url for getMe
+    response = fetch(url, session).json()
+    if response['ok']:
+        log.info('Success: {}'.format(response['result']))
+        return response['result']
+    else:  # Usually means the token is wrong
+        log.error('Error fetching bot info\nResponse: {}\nShutting Down'.format(response))
+        sys.exit()
