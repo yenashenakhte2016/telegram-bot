1a2d49585dc2a1d80344505b5ec1909b4788a351
Walid <topbakuhatsu@protonmail.ch>
2016-06-26 18:02:20 -0400

    bug fix

--- plugins/danbooru.py
+++ plugins/danbooru.py
@@ -16,13 +16,17 @@ def main(tg):
         futures = [executor.submit(create_box, tg, pic) for pic in result]
         concurrent.futures.wait(futures)
         offset = page + 1 if len(result) == 40 else ''
-        tg.answer_inline_query([box.result() for box in futures], cache_time=86400, next_offset=offset)
+        response = [box.result() for box in futures]
+        tg.answer_inline_query([box for box in response if box], cache_time=86400, next_offset=offset)
     else:
         tg.answer_inline_query([], cache_time=86400)
 
 
 def create_box(tg, pic):
-    image_url = base_url + pic['file_url']
+    try:
+        image_url = base_url + pic['file_url']
+    except KeyError:
+        return
     thumb_url = base_url + pic['preview_file_url']
     keyboard = [[{'text': 'Full Resolution', 'url': base_url + pic['large_file_url']}]]
     width = pic['image_width']
