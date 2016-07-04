6b85a63bcf53cb97d0b3a4d4a7e610128c3fb1c0
Walid <topbakuhatsu@protonmail.ch>
2016-07-04 14:31:25 -0400

    Waifu2x bug fixed + compressed image support

---
+++ plugins/autoupload.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import io
+
+
+def main(tg):
+    if 'gif' in tg.message['document']['mime_type']:
+        return
+    document_obj = tg.get_file(tg.message['document']['file_id'])
+    tg.send_chat_action('upload_photo')
+    file_path = tg.download_file(document_obj)
+    photo = Image.open(file_path)
+    photo = resize_image(photo)
+    photo = compress_image(photo)
+    name = document_obj['result']['file_id'] + ".jpg"
+    tg.send_photo((name, photo.read()),
+                  disable_notification=True,
+                  reply_to_message_id=tg.message['message_id'])
+
+
+def resize_image(image):
+    if image.size[0] > 1600 or image.size[1] > 1600:
+        larger = image.size[0] if image.size[0] > image.size[
+            1] else image.size[1]
+        scale = 1600 / larger
+        new_dimensions = (int(image.size[0] * scale),
+                          int(image.size[1] * scale))
+        return image.resize(new_dimensions)
+    return image
+
+
+def compress_image(image):
+    compressed_image = io.BytesIO()
+    image.save(compressed_image, format='JPEG')
+    compressed_image.seek(0)
+    return compressed_image
+
+
+parameters = {
+    'name': "Auto upload",
+    'short_description':
+    "Automatically uploads your uncompressed images for you",
+    'permissions': "11"
+}
+
+arguments = {'document': {'mime_type': ['image']}, 'text': ['^/upload$']}
--- plugins/waifu2x.py
+++ plugins/waifu2x.py
@@ -2,40 +2,47 @@
 
 from PIL import Image
 import io
+import re
 
 post_url = "http://waifu2x.udp.jp/api"
 
 
 def main(tg):
+    caption = ''
+    document = None
+    photo = None
     if 'reply_to_message' in tg.message and 'document' in tg.message[
             'reply_to_message']:
         document = tg.message['reply_to_message']['document']
     elif 'document' in tg.message:
         document = tg.message['document']
+    elif 'photo' in tg.message:
+        photo_id = tg.message['photo'][-1]['file_id']
     else:
         tg.send_message(
             "Reply to an image with /waifu2x or use it as a caption to upscale")
         return
-    mime_type = document['mime_type']
-    if 'image' not in mime_type:
-        tg.send_message("I can only upscale images :(")
-    elif 'gif' in mime_type:
-        tg.send_message("I can't upscale gifs :(")
-    format = mime_type.replace('image/', '')
-    photo_id = document['file_id']
+    if document:
+        mime_type = document['mime_type']
+        if 'image' not in mime_type:
+            tg.send_message("I can only upscale images :(")
+        elif 'gif' in mime_type:
+            tg.send_message("I can't upscale gifs :(")
+        photo_id = document['file_id']
     tg.send_chat_action("upload_photo")
     document_obj = tg.get_file(photo_id)
     file_path = tg.download_file(document_obj)
     file = Image.open(file_path)
     file = check_size(file)
     if file:
-        file = create_image_obj(file, format)
-        name = "{}.{}".format(photo_id, format)
+        file = create_image_obj(file)
+        name = "{}.PNG".format(photo_id)
         fields = {'file': (name, file.read()), 'noise': 2, 'scale': 2}
         x = tg.http.request('POST', post_url, fields=fields)
         tg.send_document((name, x.data))
     else:
-        tg.send_message("This image is too big for me to waifu2x :(")
+        tg.send_message("This image is too big for me to waifu2x :(",
+                        caption=caption)
 
 
 def check_size(image):
@@ -47,14 +54,14 @@ def check_size(image):
         largest = image.size[0] if image.size[0] > image.size[
             1] else image.size[1]
         scale = 1279 / largest
-        new_dimensions = (int(image.size[0] * scale), int(image.size[1] *
-                                                          scale))
+        new_dimensions = (int(image.size[0] * scale),
+                          int(image.size[1] * scale))
         return image.resize(new_dimensions, Image.LANCZOS)
 
 
-def create_image_obj(image, format):
+def create_image_obj(image):
     compressed_image = io.BytesIO()
-    image.save(compressed_image, format=format)
+    image.save(compressed_image, format="PNG")
     compressed_image.seek(0)
     return compressed_image
 
