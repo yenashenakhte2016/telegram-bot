2a439e47d66744ffa6d71a48399bb75f9a4c0251
Walid <topbakuhatsu@protonmail.ch>
2016-07-11 00:04:09 -0400

    Autouploads docstrings, 100 quality

--- plugins/autoupload.py
+++ plugins/autoupload.py
@@ -1,8 +1,15 @@
-from PIL import Image
+"""
+Uploads a compressed version of uncompressed images.
+"""
+
 import io
+from PIL import Image
 
 
 def main(tg):
+    """
+    Ignores gifs. Downloads an image, downscales, then uploads as a jpeg.
+    """
     if 'gif' in tg.message['document']['mime_type']:
         return
     document_obj = tg.get_file(tg.message['document']['file_id'])
@@ -18,6 +25,9 @@ def main(tg):
 
 
 def resize_image(image):
+    """
+    Resizes an image if its height or width > 1600. Uses lanczos downscaling.
+    """
     if image.size[0] > 1600 or image.size[1] > 1600:
         larger = image.size[0] if image.size[0] > image.size[
             1] else image.size[1]
@@ -29,8 +39,11 @@ def resize_image(image):
 
 
 def compress_image(image):
+    """
+    Saves a jpeg copy of the image in a BytesIO object with quality set to 100.
+    """
     compressed_image = io.BytesIO()
-    image.save(compressed_image, format='JPEG')
+    image.save(compressed_image, format='JPEG', quality=100)
     compressed_image.seek(0)
     return compressed_image
 
