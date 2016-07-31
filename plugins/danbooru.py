9adb9846825fbe2bd5dd895358048e58d3322f9d
Walid <topbakuhatsu@protonmail.ch>
2016-07-31 11:21:05 -0400

    Danbooru bug fixes and cleanup
    Handles UnicodeEncodeError, docstrings, cleanup

--- plugins/danbooru.py
+++ plugins/danbooru.py
@@ -1,11 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Returns inline results from danbooru
+"""
+
 import concurrent.futures
 import json
-from PIL import Image
 import io
 import os
+from PIL import Image
 
 base_url = "https://danbooru.donmai.us"
-api_key = None
 
 try:
     JSONDecodeError = json.JSONDecodeError
@@ -14,13 +18,15 @@ except AttributeError:
 
 
 def main(tg):
-    global api_key
+    """
+    Sends results from danbooru if inline or sends a redirection message
+    """
     if tg.inline_query:
         api_key = tg.config['DANBOORU']['api_key']
         page = int(tg.inline_query['offset']) if tg.inline_query['offset'] else 1
         query = "rating:s" if tg.inline_query['matched_regex'] == inline_arguments[0] else tg.inline_query['match'][1]
         query = query.split(',')
-        result = get_post(tg.http, query, page)
+        result = get_post(tg.http, query, page, api_key)
         if result:
             executor = concurrent.futures.ThreadPoolExecutor(max_workers=8)
             futures = [executor.submit(create_box, tg, pic) for pic in result]
@@ -32,10 +38,10 @@ def main(tg):
             tg.answer_inline_query([], cache_time=0)
     elif tg.message['pm_parameter']:
         tg.send_chat_action("upload_photo")
-        id = tg.message['pm_parameter'].replace('danbooru', '')
-        photo = return_photo(tg.http, id)
+        photo_id = tg.message['pm_parameter'].replace('danbooru', '')
+        photo = return_photo(tg.http, photo_id)
         if photo:
-            photo_name = "{}.png".format(id)
+            photo_name = "{}.png".format(photo_id)
             file_obj = io.BytesIO()
             photo.save(file_obj, format='PNG')
             file_obj.seek(0)
@@ -49,6 +55,9 @@ def main(tg):
 
 
 def create_box(tg, pic):
+    """
+    Creates an inline result photo
+    """
     try:
         image_url = base_url + pic['file_url']
     except KeyError:
@@ -68,23 +77,33 @@ def create_box(tg, pic):
                                         reply_markup=tg.inline_keyboard_markup(keyboard))
 
 
-def get_post(http, tags, page):
+def get_post(http, tags, page, api_key):
+    """
+    Fetches results from the given tags
+    """
     tags[-1] = get_tags(http, tags[-1])
     tags = ','.join(tags)
     fields = {'api_key': api_key, 'limit': 40, 'tags': tags.replace(' ', '_') + ' rating:safe', 'page': page}
-    request = http.request('GET', base_url + '/posts.json', fields=fields)
+    try:
+        request = http.request('GET', base_url + '/posts.json', fields=fields)
+    except UnicodeEncodeError:
+        return
     if request.status == 200:
         try:
             return json.loads(request.data.decode('UTF-8'))
         except JSONDecodeError:
             return
-    else:
-        return
 
 
 def get_tags(http, query):
+    """
+    Returns the top search result for the inputted tag
+    """
     url = base_url + "/tags/autocomplete.json?search[name_matches]={}*".format(query)
-    request = http.request('GET', url)
+    try:
+        request = http.request('GET', url)
+    except UnicodeEncodeError:
+        return query
     if request.status == 200:
         try:
             result = json.loads(request.data.decode('UTF-8'))
@@ -97,14 +116,17 @@ def get_tags(http, query):
         return query
 
 
-def return_photo(http, id):
-    file_path = "data/files/danbooru/{}.png".format(id)
+def return_photo(http, photo_id):
+    """
+    Returns a photo object from danbooru
+    """
+    file_path = "data/files/danbooru/{}.png".format(photo_id)
     try:
         return Image.open(file_path)
     except FileNotFoundError:
         pass
     url = base_url + "/posts/{}.json"
-    request = http.request('GET', url.format(id))
+    request = http.request('GET', url.format(photo_id))
     if request.status == 200:
         try:
             result = json.loads(request.data.decode('UTF-8'))
@@ -114,6 +136,9 @@ def return_photo(http, id):
 
 
 def download_photo(http, url, file_path):
+    """
+    Downloads a photo and returns the file object
+    """
     request = http.request('GET', base_url + url)
     if request.status == 200:
         image_obj = io.BytesIO(request.data)
