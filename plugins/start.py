07c641b19947400ec6d03f155a15547182f83184
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-08 01:39:10 +0430

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -4,8 +4,8 @@
 def main(tg):
     tg.send_chat_action('typing')
     first_name = tg.get_me['result']['first_name']
-    message = "Hi, I'm {}! A multipurpose telegram bot written in python 3. You can see a list of my commands using " \
-              "/help. You can also view my source code <a href='https://github.com/TopBakuhatsu/hitagibot'>here</a>" \
+    message = "Hi, I'm {}! A multipurpose telegram bot. You can see a list of my commands using " \
+              "/help. You can also view my Admin <a href='https://telegram.me/My_Mohammad'>here Admin</a>" \
         .format(first_name)
     if 'new_chat_participant' in tg.message:
         tg.send_message(message, disable_web_page_preview=True, reply_to_message_id=None)
